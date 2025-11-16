ebeb793580fdf7b2e672130d040977c08374446a
yuca-profiler <yuca-profiler@proton.me>
2025-11-15 18:56:48 -0700

    adding missing metadata

--- benchmarks/src/main/java/yuca/benchmarks/YucaRenaissancePlugin.java
+++ benchmarks/src/main/java/yuca/benchmarks/YucaRenaissancePlugin.java
@@ -1,17 +1,15 @@
 package yuca.benchmarks;
 
-import java.util.ArrayList;
 import org.renaissance.Plugin;
 import yuca.YucaMonitor;
 import yuca.benchmarks.util.YucaUtil;
 import yuca.signal.Report;
 
 public final class YucaRenaissancePlugin
-    implements Plugin.BeforeBenchmarkTearDownListener,
-        Plugin.AfterOperationSetUpListener,
-        Plugin.BeforeOperationTearDownListener {
+    implements Plugin.AfterOperationSetUpListener, Plugin.BeforeOperationTearDownListener {
   private final YucaMonitor yuca = YucaUtil.createYuca();
-  private final ArrayList<Report> reports = new ArrayList<>();
+
+  // private final ArrayList<Report> reports = new ArrayList<>();
 
   @Override
   public void afterOperationSetUp(String benchmark, int opIndex, boolean isLastOp) {
@@ -20,13 +18,27 @@ public final class YucaRenaissancePlugin
 
   @Override
   public void beforeOperationTearDown(String benchmark, int opIndex, long durationNanos) {
-    yuca.stop().ifPresent(reports::add);
-    YucaUtil.summary(reports.get(reports.size() - 1));
+    yuca.stop()
+        .ifPresent(
+            report -> {
+              YucaUtil.summary(report);
+              YucaUtil.writeReport(
+                  report.toBuilder()
+                      .addMetadata(
+                          Report.Metadata.newBuilder()
+                              .setName("iteration")
+                              .setValue(Integer.toString(opIndex)))
+                      .addMetadata(
+                          Report.Metadata.newBuilder().setName("suite").setValue("renaissance"))
+                      .addMetadata(
+                          Report.Metadata.newBuilder().setName("workload").setValue(benchmark))
+                      .build());
+            });
   }
 
-  @Override
-  public void beforeBenchmarkTearDown(String benchmark) {
-    YucaUtil.writeReports(reports);
-    reports.clear();
-  }
+  // @Override
+  // public void beforeBenchmarkTearDown(String benchmark) {
+  //   YucaUtil.writeReports(reports);
+  //   reports.clear();
+  // }
 }
