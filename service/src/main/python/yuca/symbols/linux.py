bb671dcb41d6421743ce83d5ef84a3784c098d40
yuca-profiler <yuca-profiler@proton.me>
2026-03-01 00:00:33 -0500

    what

--- service/src/main/python/yuca/symbols/linux.py
+++ service/src/main/python/yuca/symbols/linux.py
@@ -306,3 +306,5 @@ def aggregate_symbols(symbols):
         start = agg_symbols['data'][symbol]['start']
         end = agg_symbols['data'][symbol]['end']
         agg_symbols['data'][symbol]['elapsed'] = end - start
+    return agg_symbols
+    
